be7e360e29f9c3b10e3a60c93c51238b08655a0f
Kayla <kaylavd99@gmail.com>
2025-10-02 20:49:20 -0400

    Updated testing to uniformly test any directory and it's python files. If a folder contain python code, similar to converter and calculator, this program should be able to read the file contents, run the files, edit the files, and retrieve information about the directory itself

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import json
+from default_api import default_api
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    output = ''
+    error = ''
+    if request.method == "POST":
+        command = request.form["command"]
+        try:
+            # Call the get_files_info function
+            result = default_api.get_files_info(directory=command)
+            output = json.dumps(result)
+        except Exception as e:
+            error = str(e)
+
+        return render_template("index.html", command=command, output=output, error=error)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 MAXCHARS = 10000
-WORKING_DIR = "./calculator"
+WORKING_DIR = "./"
 MAX_ITERS = 20
