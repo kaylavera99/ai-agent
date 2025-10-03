f7ba0fa41edde8dd533cf56ca84820f1569d8eaa
Kayla <kaylavd99@gmail.com>
2025-10-02 20:46:13 -0400

    Updated root test file to test both apps - calculator.py and converter.py

--- tests.py
+++ tests.py
@@ -1,7 +1,28 @@
 from functions.run_python import run_python_file
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
 
-print(run_python_file("calculator", "main.py"))
-print(run_python_file("calculator", "main.py", ["3 + 5"]))
-print(run_python_file("calculator", "tests.py"))
-print(run_python_file("calculator", "../main.py"))
-print(run_python_file("calculator", "nonexistent.py"))
+print("Testing the 'run_pytyhon_file' function with various scenarios:")
+print("CALCULATOR TESTS")
+print(run_python_file(working_directory=".", file_path="calculator/main.py"))
+print(run_python_file(working_directory=".", file_path="calculator/main.py", args=["3 + 5"]))
+print(run_python_file(working_directory=".", file_path="calculator/tests.py"))
+
+print("CONVERTER TESTS")
+print(run_python_file(working_directory=".", file_path="converter/main.py"))
+print(run_python_file(working_directory=".", file_path="converter/main.py", args=['convert 10 meters to feet']))
+print(run_python_file(working_directory=".", file_path="converter/tests.py"))
+
+print("Running python files completed.\n")
+
+print("Testing get_file_content function with various scenarios:")
+print(get_file_content(working_directory=".", file_path="calculator/main.py"))
+print(get_file_content(working_directory=".", file_path="converter/main.py"))
+
+print("Getting file content completed.\n")
+
+print("Testing get_file_info function with various scenarios:")
+print(get_files_info(working_directory=".", directory="calculator"))
+print(get_files_info(working_directory=".", directory="converter"))
+
+print("Getting file info completed.\n")
